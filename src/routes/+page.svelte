6e828996338594e8c724f3e71d0b16a3847c646b
JakeOJeff <realjakeojeff@gmail.com>
2025-07-27 00:53:42 +0530

    Initial page d

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    type Item = { id: number; content: null;};
+    let items: Item[] = [];
+</script>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<div>
+    {#each items as item (item.id)}
+    <div>
+        {item.content}
+        </div>
+        {/each}
+</div>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
